5aa5b4ec72d78bbeff3120b923bf7d5f0a86ca32
Sergei Zheludkov <sergozheludkov@yandex.ru>
2019-12-26 15:52:03 +0300

    Finish third step

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
+import { userName } from '../index.js'
+
+console.log(`Welcome to the Brain Games!`);
+console.log(`Hello, ${userName()}!`);
