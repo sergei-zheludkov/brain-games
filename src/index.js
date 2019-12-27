3447a0bc1e39b8d772defd411f06e67e31593e7a
Sergei Zheludkov <sergozheludkov@yandex.ru>
2019-12-27 10:01:33 +0300

    Finish fourth step v3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+/* eslint-disable import/prefer-default-export */
+
 import readlineSync from 'readline-sync';
 
 export const userName = () => readlineSync.question('May I have your name? ');
